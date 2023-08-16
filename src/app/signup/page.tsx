1059a3aa8a2141a810c82ad14f05a9c03257664e
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2023-08-17 00:45:37 +0530

    initial commit

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,6 @@
 export default function SignUp() {
   return (
-    <main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Sign Up</h1>
     </main>
   );
