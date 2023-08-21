3dc75e910d67becef8c68a0c9157e90e482130c1
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2023-08-22 00:43:37 +0530

    initial commit

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,7 +2,8 @@
 import "@/app/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider, useTheme } from "next-themes";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,20 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
   return (
     <div lang="en">
       <section className={inter.className}>
--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -54,6 +54,7 @@ export const Header = () => {
         >
           <div className="flex flex-col gap-y-4 gap-x-0 mt-5 md:flex-row md:items-center md:justify-end md:gap-y-0 md:gap-x-7 md:mt-0 md:pl-7">
             <ThemeSwitcher />
+            {/* <ThemeSwitch/> */}
             <a
               className="font-medium text-blue-600 md:py-6 dark:text-blue-500"
               href="#"
@@ -179,7 +180,7 @@ export const Header = () => {
               >
                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
               </svg>
-              Log in
+              Sign in
             </a>
           </div>
         </div>
