0396a4a8fb6d5b9188ce50c6cac9ba5ba64b2d55
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2023-08-26 12:40:26 +0530

    initial commit

--- src/app/(auth)/forgotpassword/page.tsx
+++ src/app/(auth)/forgotpassword/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function ForgotPassword() {
   return (
     <main className="w-full max-w-md mx-auto p-6">
-      <div className="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+      <div className="mt-7 bg-slate-50 border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
         <div className="p-4 sm:p-7">
           <div className="text-center">
             <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">
