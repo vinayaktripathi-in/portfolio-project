525dc1f9758f038766c990ed551b52a9322f1766
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2023-09-09 16:56:34 +0530

    initial commit

--- app/(auth)/forgotpassword/page.tsx
+++ app/(auth)/forgotpassword/page.tsx
@@ -94,32 +94,13 @@ export default function ForgotPassword() {
                       type="email"
                       id="email"
                       name="email"
-                      className="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
+                      className="py-3 px-4 block w-full border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
                       aria-describedby="email-error"
                     />
                     {errors.email && (
                       <p className="text-red-600">Email is required.</p>
                     )}
-                    <div className="hidden absolute inset-y-0 right-0 items-center pointer-events-none pr-3">
-                      <svg
-                        className="h-5 w-5 text-red-500"
-                        width={16}
-                        height={16}
-                        fill="currentColor"
-                        viewBox="0 0 16 16"
-                        aria-hidden="true"
-                      >
-                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                      </svg>
-                    </div>
                   </div>
-                  <p
-                    className="hidden text-xs text-red-600 mt-2"
-                    id="email-error"
-                  >
-                    Please include a valid email address so we can get back to
-                    you
-                  </p>
                 </div>
                 {/* End Form Group */}
                 <button
