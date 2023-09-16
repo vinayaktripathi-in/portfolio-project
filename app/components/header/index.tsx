830cd7ad8be1410e7856bfc01999d3860302b2c7
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2023-09-16 17:14:44 +0530

    initial commit

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -196,7 +196,7 @@ export const Header = () => {
       {hamburger && (
         <>
           <div
-            className={`block overflow-hidden transition-all duration-300 basis-full grow md:hidden`}
+            className={`w-56 block absolute top-20 right-5 md:shadow-md p-2 bg-white dark:bg-gray-800 md:dark:border dark:border-gray-700 dark:divide-gray-700 rounded-lg overflow-hidden transition-all duration-300 basis-full grow md:hidden`}
           >
             <div className="p-2 flex flex-col items-center gap-y-4 gap-x-0 mt-5">
               <ThemeSwitch />
@@ -231,90 +231,6 @@ export const Header = () => {
               >
                 Blog
               </Link>
-              <div className="md:py-4">
-                <button
-                  onClick={() => setDropdown(!dropdown)}
-                  type="button"
-                  className="flex items-center w-full text-gray-500 hover:text-gray-400 font-medium dark:text-gray-400 dark:hover:text-gray-500 "
-                >
-                  Dropdown
-                  <MdOutlineKeyboardArrowDown />
-                </button>
-                {dropdown && (
-                  <div className="absolute top-16 right-24 transition-[opacity,margin] duration-[0.1ms] md:duration-[150ms] md:w-48 z-10 bg-white md:shadow-md rounded-lg p-2 dark:bg-gray-800 md:dark:border dark:border-gray-700 dark:divide-gray-700 md:border">
-                    <Link
-                      className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      About
-                    </Link>
-                    <div className="hs-dropdown relative [--strategy:static] md:[--strategy:absolute] [--adaptive:none] md:[--trigger:hover]">
-                      <button
-                        type="button"
-                        className="w-full flex justify-between items-center text-sm text-gray-800 rounded-md py-2 px-3 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      >
-                        Sub Menu
-                        <svg
-                          className="md:-rotate-90 ml-2 w-2.5 h-2.5 text-gray-600"
-                          width={16}
-                          height={16}
-                          viewBox="0 0 16 16"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
-                            stroke="currentColor"
-                            strokeWidth={2}
-                            strokeLinecap="round"
-                          />
-                        </svg>
-                      </button>
-                      <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] md:duration-[150ms] hs-dropdown-dropdown:opacity-100 opacity-0 md:w-48 hidden z-10 md:mt-2 bg-white md:shadow-md rounded-lg p-2 dark:bg-gray-800 md:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute md:border before:-right-5 before:top-0 before:h-full before:w-5 top-0 right-full !mx-[10px]">
-                        <Link
-                          className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                          href="#"
-                        >
-                          About
-                        </Link>
-                        <Link
-                          className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                          href="#"
-                        >
-                          Downloads
-                        </Link>
-                        <Link
-                          className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                          href="#"
-                        >
-                          Team Account
-                        </Link>
-                        <div className="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 md:border-gray-300 dark:border-gray-700 dark:text-gray-400 dark:hover:text-blue-500">
-                          <AiOutlineUser />
-                          <button
-                            className="font-medium text-gray-500 hover:text-gray-400 md:py-6 dark:text-gray-400 dark:hover:text-gray-500"
-                            onClick={handleSignOut}
-                          >
-                            Sign Out
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                    <Link
-                      className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      Downloads
-                    </Link>
-                    <Link
-                      className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                      href="#"
-                    >
-                      Team Account
-                    </Link>
-                  </div>
-                )}
-              </div>
               <Link
                 className="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 md:border-l md:border-gray-300 md:my-6 md:pl-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-blue-500"
                 href={data ? "/profile" : "/signin"}
@@ -328,6 +244,17 @@ export const Header = () => {
                   "Sign in"
                 )}
               </Link>
+              {data && (
+                <div className="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 md:border-gray-300 dark:border-gray-700 dark:text-gray-400 dark:hover:text-blue-500">
+                  <AiOutlineUser />
+                  <button
+                    className="font-medium text-gray-500 hover:text-gray-400 md:py-6 dark:text-gray-400 dark:hover:text-gray-500"
+                    onClick={handleSignOut}
+                  >
+                    Sign Out
+                  </button>
+                </div>
+              )}
             </div>
           </div>
         </>
